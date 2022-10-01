11924a572416cd7e90f0c7fe5dfe2dade63f651c
RenJah <khazix1313@gmail.com>
2022-10-01 22:50:36 +0300

    docker added

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask
+
+from db import db
+
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASSWORD}@db/{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    return app
